5f8b36336795e8845974929e687306c74f76e867
A. Handless <algorithmpit@gmail.com>
2017-02-04 04:27:35 +1100

    extract code

---
+++ LocalMelodyGenerator/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telegram;
+using MelodyGeneratorBot;
+using Newtonsoft.Json.Linq;
+
+namespace LocalMelodyGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Task _botTask;
+        private User _botInfo;
+        private CancellationTokenSource _cts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_botTask == null || _botTask.IsCompleted)
+            {
+                StartBot();
+                StartButton.Content = "Stop";
+            }
+            else
+            {
+                EndBot();
+                StartButton.Content = "Start";
+            }
+        }
+
+        private async void StartBot()
+        {
+            var apiToken = this.ApiTokenTextBox.Password;
+            _cts = new CancellationTokenSource();
+            var bot = new MelodyGenerationBot(apiToken);
+            _botTask = Task.Run(async delegate ()
+            {
+                var botapi = new BotApi(apiToken);
+                try
+                {
+                    _botInfo = await botapi.GetMe();
+                }
+                catch
+                {
+                    StartButton.Dispatcher.Invoke(() => StartButton.Content = "Start");
+                }
+
+                this.LogScrollViewer.Dispatcher.Invoke(delegate ()
+                {
+                    TextBlock tb = new TextBlock();
+                    var msg = "The bot has been started";
+                    if (_botInfo == null)
+                        msg = "The bot cannot be started";
+                    tb.Text = $"{DateTime.Now.ToLongTimeString()} {msg}";
+                    LogPanel.Children.Add(tb);
+                });
+                if (_botInfo == null)
+                    return;
+                int maxUpdate = 0;
+                while (!_cts.Token.IsCancellationRequested)
+                {
+                    Update[] updates = null;
+                    try
+                    {
+                        updates = await botapi.GetUpdates(maxUpdate);
+                        if (updates.Length > 0)
+                            maxUpdate = updates.Select(u => u.Id).Max() + 1;
+                    }
+                    catch (Exception)
+                    {
+                        break;
+                    }
+                    foreach (var update in updates)
+                    {
+                        bot.ProcessUpdate(update);
+                        this.LogPanel.Dispatcher.Invoke(delegate ()
+                        {
+                            var notification = new TextBlock
+                            {
+                                Text = $"{DateTime.Now.ToLongTimeString()} Update {update.Id} processed"
+                            };
+                            LogPanel.Children.Add(notification);
+                            //Update contents
+                            var spoiler = new Expander();
+
+                            var updateJson = new TextBox
+                            {
+                                Text = JObject.FromObject(update).ToString(),
+                                FontFamily = new FontFamily("Consolas"),
+                                IsReadOnly = true
+                            };
+                            spoiler.Content = updateJson;
+                            spoiler.Header = "Update JSON";
+                            LogPanel.Children.Add(spoiler);
+                        });
+                    }
+                }
+                this.LogScrollViewer.Dispatcher.Invoke(delegate ()
+                {
+                    TextBlock tb = new TextBlock();
+                    tb.Text = $"{DateTime.Now.ToLongTimeString()} The bot has been stopped";
+                    LogPanel.Children.Add(tb);
+                });
+
+            }, _cts.Token);
+
+        }
+
+        private async void EndBot()
+        {
+            _cts?.Cancel();
+            if (_botTask != null)
+                await _botTask;
+        }
+
+        private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            EndBot();
+        }
+    }
+}
---
+++ MelodyGeneratorBot/MelodyGenerationBot.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Telegram;
+using Simue;
+using System.Threading.Tasks;
+using NAudio.Lame;
+using NAudio.Wave;
+using WaveGenerator;
+
+namespace MelodyGeneratorBot
+{
+    public class MelodyGenerationBot : Bot
+    {
+        private BotApi _api;
+        SimueCompiler _compiler = new SimueCompiler();
+
+        public MelodyGenerationBot(string apiToken)
+        {
+            _api = new BotApi(apiToken);
+            this.MessageRecieved += MelodyGenerationBot_MessageRecieved;
+        }
+
+        private void MelodyGenerationBot_MessageRecieved(Message message)
+        {
+            var result = _compiler.Parse(_compiler.Tokenize(message.Text.Trim(' ').ToLower()));
+            if (result.Errors.Count == 0)
+            {
+                var song = result.Song;
+                song.Name = message.Text;
+                int maxMin = 20;
+                if (song.Length > TimeSpan.FromMinutes(maxMin).TotalMilliseconds)
+                    _api.SendMessage(message.Chat, $"Ваша мелодия слишком длинная. Разрешена генерация мелодий не длиннее {maxMin} минут.").Wait();
+                else
+                    SendSong(_api, message.Chat, song).Wait();
+            }
+            else
+            {
+                _api.SendMessage(message.Chat, $"Не могу понять вашу мелодию.").Wait();
+                _api.SendMessage(message.Chat, $"В коде мелодии были обнаружены ошибки, а именно:{Environment.NewLine}" +
+                                      string.Join($";{Environment.NewLine}",
+                                          result.Errors.Select(token => $"• на позиции {token.Index}: <i>{token.Value}</i>")) + ".").Wait();
+            }
+
+        }
+        private async Task SendSong(BotApi botapi, Chat chat, Song song)
+        {
+            var generationTaskTask = Task.Run<byte[]>(() => GenerateMp3(song));
+            do
+            {
+                await botapi.SendChatAction(chat, "upload_audio");
+            } while (!generationTaskTask.Wait(4000));
+            if (generationTaskTask.Result == null)
+            {
+                await botapi.SendMessage(chat, $"Во время генерации аудиофайла произошла ошибка.");
+                return;
+            }
+            var stream = new MemoryStream(generationTaskTask.Result);
+            var name = new string(song.Name.Take(100).ToArray());
+            try
+            {
+                await botapi.SendChatAction(chat, "upload_audio");
+                var sendingTask = Task.Run<Message>(
+                    async () => await botapi.SendAudio(chat, stream, name + ".mp3", (int)(song.Length / 1000), name));
+                while (!sendingTask.Wait(4000))
+                    await botapi.SendChatAction(chat, "upload_audio");
+            }
+            catch
+            {
+                await botapi.SendMessage(chat, $"Во время отправки аудиофайла произошла ошибка.");
+            }
+        }
+        private byte[] GenerateMp3(Song song)
+        {
+            if (song.Notes == null)
+                return null;
+            using (var generatedSongStream = new MemoryStream())
+            {
+                var mp3File = new MemoryStream();
+                var waveFile = new WaveFile(22050, BitDepth.Bit16, 1, generatedSongStream);
+                var sg = new SoundGenerator(waveFile);
+                foreach (var note in song.Notes)
+                    sg.AddSimpleTone(note.Frequency, note.Duration);
+                sg.Save();
+                generatedSongStream.Position = 0;
+                var r = new WaveFileReader(generatedSongStream);
+                var wr = new LameMP3FileWriter(mp3File, r.WaveFormat, 96);
+                r.CopyTo(wr);
+                wr.Flush();
+                var fileData = mp3File.ToArray();
+                mp3File.Dispose();
+                return fileData;
+            }
+        }
+    }
+}
